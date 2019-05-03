4c74b44839a52ab5ee1d0976772e55f4ed0cd1c1
Phillip Whelan <phillip.whelan@timestamp.cl>
2019-05-03 13:35:38 -0400

    FIX: respond to MX queries, send errors for unimplemented RRs.

--- dns.go
+++ dns.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 
 	"github.com/gomodule/redigo/redis"
 	"github.com/miekg/dns"
@@ -31,9 +32,8 @@ func (g *GynDNS) runDNS(ctxt context.Context, errChan chan error) {
 
 func (g *GynDNS) ServeDNS(rw dns.ResponseWriter, r *dns.Msg) {
 	for _, q := range r.Question {
-		if q.Qtype != dns.TypeA {
-			log.Printf("Unsupported question type %d", q.Qtype)
-		} else {
+		switch q.Qtype {
+		case dns.TypeMX:
 			log.Printf("Searching for hostname '%s'", q.Name)
 			response := &dns.Msg{
 				MsgHdr: dns.MsgHdr{
@@ -47,28 +47,78 @@ func (g *GynDNS) ServeDNS(rw dns.ResponseWriter, r *dns.Msg) {
 
 			r := g.pool.Get()
 			defer r.Close()
-			ip, err := redis.String(r.Do("GET", fmt.Sprintf("hostname/%s", q.Name)))
+			ip, err := redis.String(r.Do("GET",
+				fmt.Sprintf("hostname/%s", strings.ToLower(q.Name))))
 
 			if err != nil {
 				response.Rcode = dns.RcodeNameError
 				log.Printf("ERROR: %s\n", err)
 				log.Println("Hostname " + q.Name + " not found in map")
+			} else {
+				response.Answer = append(response.Answer, &dns.MX{
+					Hdr: dns.RR_Header{
+						Name:   q.Name,
+						Rrtype: dns.TypeMX,
+						Class:  dns.ClassINET,
+						Ttl:    TTL,
+					},
+					Mx: strings.ToLower(q.Name),
+				})
+				log.Println(q.Name + " A " + ip)
 			}
-			ipaddr := net.ParseIP(ip)
-			response.Answer = append(response.Answer, &dns.A{
-				Hdr: dns.RR_Header{
-					Name:   q.Name,
-					Rrtype: dns.TypeA,
-					Class:  dns.ClassINET,
-					Ttl:    TTL,
-				},
-				A: ipaddr,
-			})
-			log.Println(q.Name + " A " + ip)
 
 			rw.WriteMsg(response)
-
 			break
+		case dns.TypeA:
+			log.Printf("Searching for hostname '%s'", q.Name)
+			response := &dns.Msg{
+				MsgHdr: dns.MsgHdr{
+					Id:            r.Id,
+					Response:      true,
+					Authoritative: true,
+				},
+			}
+
+			response.Question = append(response.Question, q)
+
+			r := g.pool.Get()
+			defer r.Close()
+			ip, err := redis.String(r.Do("GET",
+				fmt.Sprintf("hostname/%s", strings.ToLower(q.Name))))
+
+			if err != nil {
+				response.Rcode = dns.RcodeNameError
+				log.Printf("ERROR: %s\n", err)
+				log.Println("Hostname " + q.Name + " not found in map")
+			} else {
+				ipaddr := net.ParseIP(ip)
+				response.Answer = append(response.Answer, &dns.A{
+					Hdr: dns.RR_Header{
+						Name:   q.Name,
+						Rrtype: dns.TypeA,
+						Class:  dns.ClassINET,
+						Ttl:    TTL,
+					},
+					A: ipaddr,
+				})
+				log.Println(q.Name + " A " + ip)
+			}
+
+			rw.WriteMsg(response)
+			break
+		default:
+			log.Printf("Unsupported question type %d", q.Qtype)
+			log.Printf("%+v\n", q)
+			response := &dns.Msg{
+				MsgHdr: dns.MsgHdr{
+					Id:            r.Id,
+					Response:      false,
+					Authoritative: false,
+				},
+			}
+			response.Rcode = dns.RcodeNotImplemented
+			rw.WriteMsg(response)
+			//response.Rcode = dns.RcodeNameError
 		}
 	}
 }
